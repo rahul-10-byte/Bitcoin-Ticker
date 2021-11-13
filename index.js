60f73c1f2e434ffa5bf9f1b12331c1d870aaadf9
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-11-13 09:45:28 +0530

    Created Server

---
+++ index.js
@@ -0,0 +1,16 @@
+// jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,  res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+    console.log("Server started at 3000!");
+});

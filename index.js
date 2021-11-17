7e12c911d0ae6fe6d12d4a176d86a13c77570ebb
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-11-17 22:22:06 +0530

    Route Resolved

--- index.js
+++ index.js
@@ -16,16 +16,23 @@ app.post("/", function (req, res) {
     
     console.log(req.body.crypto);
 
-    
+    // var crypto = req.body.crypto;
+    // var fiat = req.body.fiat;
 
-    var baseURL = "https://api.nomics.com/v1/currencies/ticker?key=ae62b83e138193bccc72a9ab6a13719864c3b678&ids=BTC&interval=1d&convert=USD&per-page=100&page=1";
+    var baseURL = "https://api.nomics.com/v1/currencies/ticker?key=ae62b83e138193bccc72a9ab6a13719864c3b678";
+    var coin = "&ids=" + crypto;
+    var currency = "&interval=1d&convert="+ fiat + "&per-page=100&page=1";
 
-    request(baseURL, function(error, response, body) {
+    var completeURL = baseURL + coin + currency;  
+
+    console.log(completeURL); //for API testing
+
+    request(completeURL, function(error, response, body) {
         
         var data = JSON.parse(body);
         var price = data[0].price;
         console.log(Number(price));
-        res.send("<h1>The price of the Bitcoin is " + price + " USD</h1>");
+        res.send("<h1>The price of the " + crypto + " is " + price +" " + fiat + " </h1>");
 
     });
 
